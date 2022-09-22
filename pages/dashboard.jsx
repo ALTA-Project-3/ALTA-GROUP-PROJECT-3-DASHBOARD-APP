f1f83c6907309b779a080125fd75728132ebe238
Raka Fantino <rakafantino@gmail.com>
2022-09-23 02:22:17 +0700

    sedang mencoba menyelesaikan masalah depoloyment

--- pages/dashboard.jsx
+++ pages/dashboard.jsx
@@ -5,6 +5,7 @@ import React, { useEffect, useState } from "react";
 import { Line } from "react-chartjs-2";
 import Header from "../components/Header";
 import SideBar from "../components/SideBar";
+import Chart from "chart.js/auto";
 
 export default function Dashboard() {
   const [total, setTotal] = useState([]);
