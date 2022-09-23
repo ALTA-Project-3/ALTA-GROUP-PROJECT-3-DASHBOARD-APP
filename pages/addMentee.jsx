9b0fc470d824b679a49299fe315b274641b9b071
Raka Fantino <rakafantino@gmail.com>
2022-09-23 14:11:03 +0700

    Add mentee layout Revision

--- pages/addMentee.jsx
+++ pages/addMentee.jsx
@@ -50,11 +50,26 @@ const AddMentee = () => {
                 </div>
               </label>
 
-              <label className=" text-sm flex">
-                <span className="text-gray-700 dark:text-gray-400 pt-3 basis-1/6">Gender</span>
-                <div className="relative text-gray-500 focus-within:text-purple-600 dark:focus-within:text-purple-400 basis-4/5">
+              <label className="text-sm flex my-3">
+                <span className="text-gray-700 dark:text-gray-400 basis-1/6">Gender</span>
+                <div className="relative text-gray-500 focus-within:text-purple-600 dark:focus-within:text-purple-400 items-center flex gap-3 ">
+                  <label for="male">Male</label>
                   <input
-                    className=" w-full mt-1 text-sm rounded-md mb-1 text-black dark:text-gray-300 dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:focus:shadow-outline-gray form-input"
+                    id="male"
+                    name="gender"
+                    value="Male"
+                    type="radio"
+                    className="rounded-full border-gray-600 bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:focus:shadow-outline-gray form-radio"
+                    placeholder="Gender"
+                  />
+
+                  <label for="female">Female</label>
+                  <input
+                    id="female"
+                    name="gender"
+                    value="Female"
+                    type="radio"
+                    className="rounded-full border-gray-600 bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:focus:shadow-outline-gray form-radio"
                     placeholder="Gender"
                   />
                 </div>
@@ -114,12 +129,27 @@ const AddMentee = () => {
 
               <p className="text-gray-700 dark:text-gray-400 pt-3 font-bold">Education Data</p>
 
-              <label className=" text-sm flex">
-                <span className="text-gray-700 dark:text-gray-400 pt-3 basis-1/6">Type</span>
-                <div className="relative text-gray-500 focus-within:text-purple-600 dark:focus-within:text-purple-400 basis-4/5">
+              <label className=" text-sm flex mt-3">
+                <span className="text-gray-700 dark:text-gray-400 basis-1/6">Type</span>
+                <div className="relative text-gray-500 focus-within:text-purple-600 dark:focus-within:text-purple-400 basis-4/5 flex gap-3 items-center mb-3">
+                  <label for="Informatic">Informatic</label>
                   <input
-                    className=" w-full mt-1 text-sm rounded-md mb-1 text-black dark:text-gray-300 dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:focus:shadow-outline-gray form-input"
-                    placeholder="Type"
+                    id="Informatic"
+                    name="gender"
+                    value="Informatic"
+                    type="radio"
+                    className="rounded-full border-gray-600 bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:focus:shadow-outline-gray form-radio"
+                    placeholder="Gender"
+                  />
+
+                  <label for="Non-Informatic">Non-Informatic</label>
+                  <input
+                    id="Non-Informatic"
+                    name="gender"
+                    value="Non-Informatic"
+                    type="radio"
+                    className="rounded-full border-gray-600 bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:focus:shadow-outline-gray form-radio"
+                    placeholder="Gender"
                   />
                 </div>
               </label>
