645097ee65668b38e616eb64a546dd0dadad2345
unknown <syifaulya0610@gmail.com>
2022-09-23 12:03:27 +0700

    revisi list mentee page

--- pages/addMentee.jsx
+++ pages/addMentee.jsx
@@ -1,212 +1,209 @@
-import Header from "../components/Header"
-import SideBar from "../components/SideBar"
+import Header from "../components/Header";
+import SideBar from "../components/SideBar";
 
 const AddMentee = () => {
   return (
     <>
-    <div className="flex h-screen bg-gray-900 ">
+      <div className="flex h-screen bg-gray-900 h-max">
         <SideBar />
-        <div className="w-screen">
+        <div className="w-screen ">
           <Header />
           <div className="px-10 py-10">
-          <div
-              className="px-4 py-3 mb-8 bg-white rounded-lg shadow-md dark:bg-gray-800 "
-            >
+            <div className="px-8 py-3 mb-8 bg-white rounded-lg shadow-md dark:bg-gray-800 ">
               <label className="block text-sm flex">
-                <span className="text-gray-700 dark:text-gray-400 pt-3 basis-1/6">Name</span>
-                <div
-                  className="relative text-gray-500 focus-within:text-purple-600 dark:focus-within:text-purple-400 basis-4/5"
-                >
+                <span className="text-gray-700 dark:text-gray-400 pt-3 basis-1/6">
+                  Name
+                </span>
+                <div className="relative text-gray-500 focus-within:text-purple-600 dark:focus-within:text-purple-400 basis-4/5 ">
                   <input
-                    className="block w-full mt-1 text-sm text-black dark:text-gray-300 dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:focus:shadow-outline-gray form-input"
+                    className="block rounded-lg w-full mt-1 text-sm text-black dark:text-gray-300 dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:focus:shadow-outline-gray form-input"
                     placeholder="Name"
                   />
-                
                 </div>
               </label>
 
               <label className="block text-sm flex">
-                <span className="text-gray-700 dark:text-gray-400 pt-3 basis-1/6">Address</span>
-                <div
-                  className="relative text-gray-500 focus-within:text-purple-600 dark:focus-within:text-purple-400 basis-4/5"
-                >
+                <span className="text-gray-700 dark:text-gray-400 pt-3 basis-1/6">
+                  Address
+                </span>
+                <div className="relative text-gray-500 focus-within:text-purple-600 dark:focus-within:text-purple-400 basis-4/5">
                   <input
-                    className="block w-full mt-1 text-sm text-black dark:text-gray-300 dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:focus:shadow-outline-gray form-input"
+                    className="block rounded-lg w-full mt-1 text-sm text-black dark:text-gray-300 dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:focus:shadow-outline-gray form-input"
                     placeholder="Adress"
                   />
-                
                 </div>
               </label>
 
               <label className="block text-sm flex">
-                <span className="text-gray-700 dark:text-gray-400 pt-3 basis-1/6">Home Adress</span>
-                <div
-                  className="relative text-gray-500 focus-within:text-purple-600 dark:focus-within:text-purple-400 basis-4/5"
-                >
+                <span className="text-gray-700 dark:text-gray-400 pt-3 basis-1/6">
+                  Home Adress
+                </span>
+                <div className="relative text-gray-500 focus-within:text-purple-600 dark:focus-within:text-purple-400 basis-4/5">
                   <input
-                    className="block w-full mt-1 text-sm text-black dark:text-gray-300 dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:focus:shadow-outline-gray form-input"
+                    className="block rounded-lg w-full mt-1 text-sm text-black dark:text-gray-300 dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:focus:shadow-outline-gray form-input"
                     placeholder="Home Adress"
                   />
-                
                 </div>
               </label>
 
               <label className="block text-sm flex">
-                <span className="text-gray-700 dark:text-gray-400 pt-3 basis-1/6">Email</span>
-                <div
-                  className="relative text-gray-500 focus-within:text-purple-600 dark:focus-within:text-purple-400 basis-4/5"
-                >
+                <span className="text-gray-700 dark:text-gray-400 pt-3 basis-1/6">
+                  Email
+                </span>
+                <div className="relative text-gray-500 focus-within:text-purple-600 dark:focus-within:text-purple-400 basis-4/5">
                   <input
-                    className="block w-full mt-1 text-sm text-black dark:text-gray-300 dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:focus:shadow-outline-gray form-input"
+                    className="block rounded-lg w-full mt-1 text-sm text-black dark:text-gray-300 dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:focus:shadow-outline-gray form-input"
                     placeholder="Email"
                   />
-                
                 </div>
               </label>
 
               <label className="block text-sm flex">
-                <span className="text-gray-700 dark:text-gray-400 pt-3 basis-1/6">Gender</span>
-                <div
-                  className="relative text-gray-500 focus-within:text-purple-600 dark:focus-within:text-purple-400 basis-4/5"
-                >
-                  <input
-                    className="block w-full mt-1 text-sm text-black dark:text-gray-300 dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:focus:shadow-outline-gray form-input"
-                    placeholder="Gender"
-                  />
-                
+                <span className="text-gray-700 dark:text-gray-400 pt-3 basis-1/6">
+                  Gender
+                </span>
+                <div className="dropdown pt-1 relative text-gray-500 focus-within:text-purple-600 dark:focus-within:text-purple-400 basis-4/5">
+                  <select className=" w-full  px-4 py-2 rounded-lg text-sm border border-gray-600 bg-gray-700 focus:border-purple-400  focus:outline-none  text-gray-500 focus:shadow-outline-gray" >
+                  <option value="">Select Gender</option>
+                  <option value="Male">Male</option>
+                  <option value="Female">Female</option>
+                </select>
+                  
                 </div>
               </label>
 
               <label className="block text-sm flex">
-                <span className="text-gray-700 dark:text-gray-400 pt-3 basis-1/6">Telegram</span>
-                <div
-                  className="relative text-gray-500 focus-within:text-purple-600 dark:focus-within:text-purple-400 basis-4/5"
-                >
+                <span className="text-gray-700 dark:text-gray-400 pt-3 basis-1/6">
+                  Telegram
+                </span>
+                <div className="relative text-gray-500 focus-within:text-purple-600 dark:focus-within:text-purple-400 basis-4/5">
                   <input
-                    className="block w-full mt-1 text-sm text-black dark:text-gray-300 dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:focus:shadow-outline-gray form-input"
+                    className="block rounded-lg w-full mt-1 text-sm text-black dark:text-gray-300 dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:focus:shadow-outline-gray form-input"
                     placeholder="Telegram"
                   />
-                
                 </div>
               </label>
 
               <label className="block text-sm flex">
-                <span className="text-gray-700 dark:text-gray-400 pt-3 basis-1/6">Phone</span>
-                <div
-                  className="relative text-gray-500 focus-within:text-purple-600 dark:focus-within:text-purple-400 basis-4/5"
-                >
+                <span className="text-gray-700 dark:text-gray-400 pt-3 basis-1/6">
+                  Phone
+                </span>
+                <div className="relative text-gray-500 focus-within:text-purple-600 dark:focus-within:text-purple-400 basis-4/5">
                   <input
-                    className="block w-full mt-1 text-sm text-black dark:text-gray-300 dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:focus:shadow-outline-gray form-input"
+                    className="block rounded-lg w-full mt-1 text-sm text-black dark:text-gray-300 dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:focus:shadow-outline-gray form-input"
                     placeholder="Phone"
                   />
-                
                 </div>
               </label>
 
-              <p className="text-gray-700 dark:text-gray-400 pt-3">Emergency Data</p>
+              <p className="text-gray-700 dark:text-gray-400 pt-3">
+                Emergency Data
+              </p>
 
               <label className="block text-sm flex">
-                <span className="text-gray-700 dark:text-gray-400 pt-3 basis-1/6">Name</span>
-                <div
-                  className="relative text-gray-500 focus-within:text-purple-600 dark:focus-within:text-purple-400 basis-4/5"
-                >
+                <span className="text-gray-700 dark:text-gray-400 pt-3 basis-1/6">
+                  Name
+                </span>
+                <div className="relative text-gray-500 focus-within:text-purple-600 dark:focus-within:text-purple-400 basis-4/5">
                   <input
-                    className="block w-full mt-1 text-sm text-black dark:text-gray-300 dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:focus:shadow-outline-gray form-input"
+                    className="block rounded-lg w-full mt-1 text-sm text-black dark:text-gray-300 dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:focus:shadow-outline-gray form-input"
                     placeholder="Name"
                   />
-                
                 </div>
               </label>
 
               <label className="block text-sm flex">
-                <span className="text-gray-700 dark:text-gray-400 pt-3 basis-1/6">Phone</span>
-                <div
-                  className="relative text-gray-500 focus-within:text-purple-600 dark:focus-within:text-purple-400 basis-4/5"
-                >
+                <span className="text-gray-700 dark:text-gray-400 pt-3 basis-1/6">
+                  Phone
+                </span>
+                <div className="relative text-gray-500 focus-within:text-purple-600 dark:focus-within:text-purple-400 basis-4/5">
                   <input
-                    className="block w-full mt-1 text-sm text-black dark:text-gray-300 dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:focus:shadow-outline-gray form-input"
+                    className="block rounded-lg w-full mt-1 text-sm text-black dark:text-gray-300 dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:focus:shadow-outline-gray form-input"
                     placeholder="Phone"
                   />
-                
                 </div>
               </label>
 
               <label className="block text-sm flex">
-                <span className="text-gray-700 dark:text-gray-400 pt-3 basis-1/6">Status</span>
-                <div
-                  className="relative text-gray-500 focus-within:text-purple-600 dark:focus-within:text-purple-400 basis-4/5"
-                >
-                  <input
-                    className="block w-full mt-1 text-sm text-black dark:text-gray-300 dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:focus:shadow-outline-gray form-input"
-                    placeholder="Status"
-                  />
-                
+                <span className="text-gray-700 dark:text-gray-400 pt-3 basis-1/6">
+                  Status
+                </span>
+                <div className="relative pt-1 text-gray-500 focus-within:text-purple-600 dark:focus-within:text-purple-400 basis-4/5">
+                  <select className=" w-full  px-4 py-2 rounded-lg text-sm border border-gray-600 bg-gray-700 focus:border-purple-400  focus:outline-none  text-gray-500 focus:shadow-outline-gray">
+                    <option value="">Select Status</option>
+                    <option value="Orangtua">Orangtua</option>
+                    <option value="Kakek Nenek">Kakek Nenek</option>
+                    <option value="Saudara Orangtua">Saudara Orangtua</option>
+                  </select>
                 </div>
               </label>
 
-              <p className="text-gray-700 dark:text-gray-400 pt-3">Education Data</p>
+              <p className="text-gray-700 dark:text-gray-400 pt-3">
+                Education Data
+              </p>
 
               <label className="block text-sm flex">
-                <span className="text-gray-700 dark:text-gray-400 pt-3 basis-1/6">Type</span>
-                <div
-                  className="relative text-gray-500 focus-within:text-purple-600 dark:focus-within:text-purple-400 basis-4/5"
-                >
-                  <input
-                    className="block w-full mt-1 text-sm text-black dark:text-gray-300 dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:focus:shadow-outline-gray form-input"
-                    placeholder="Type"
-                  />
-                
+                <span className="text-gray-700 dark:text-gray-400 pt-3 basis-1/6">
+                  Type
+                </span>
+                <div className="relative pt-1 text-gray-500 focus-within:text-purple-600 dark:focus-within:text-purple-400 basis-4/5">
+                  <select className=" w-full  px-4 py-2 rounded-lg text-sm border border-gray-600 bg-gray-700 focus:border-purple-400  focus:outline-none  text-gray-500 focus:shadow-outline-gray">
+                    <option value="">Select Education</option>
+                    <option value="IT">IT</option>
+                    <option value="Not-IT">Not-IT</option>
+                  </select>
                 </div>
               </label>
 
               <label className="block text-sm flex">
-                <span className="text-gray-700 dark:text-gray-400 pt-3 basis-1/6">Class</span>
-                <div
-                  className="relative text-gray-500 focus-within:text-purple-600 dark:focus-within:text-purple-400 basis-4/5"
-                >
+                <span className="text-gray-700 dark:text-gray-400 pt-3 basis-1/6">
+                  Class
+                </span>
+                <div className="relative text-gray-500 focus-within:text-purple-600 dark:focus-within:text-purple-400 basis-4/5">
                   <input
-                    className="block w-full mt-1 text-sm text-black dark:text-gray-300 dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:focus:shadow-outline-gray form-input"
+                    className="block rounded-lg w-full mt-1 text-sm text-black dark:text-gray-300 dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:focus:shadow-outline-gray form-input"
                     placeholder="Class"
                   />
-                
                 </div>
               </label>
 
               <label className="block text-sm flex">
-                <span className="text-gray-700 dark:text-gray-400 pt-3 basis-1/6">Major</span>
-                <div
-                  className="relative text-gray-500 focus-within:text-purple-600 dark:focus-within:text-purple-400 basis-4/5"
-                >
+                <span className="text-gray-700 dark:text-gray-400 pt-3 basis-1/6">
+                  Major
+                </span>
+                <div className="relative text-gray-500 focus-within:text-purple-600 dark:focus-within:text-purple-400 basis-4/5">
                   <input
-                    className="block w-full mt-1 text-sm text-black dark:text-gray-300 dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:focus:shadow-outline-gray form-input"
+                    className="block rounded-lg w-full mt-1 text-sm text-black dark:text-gray-300 dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:focus:shadow-outline-gray form-input"
                     placeholder="Major"
                   />
-                
                 </div>
               </label>
 
               <label className="block text-sm flex">
-                <span className="text-gray-700 dark:text-gray-400 pt-3 basis-1/6">Graduate</span>
-                <div
-                  className="relative text-gray-500 focus-within:text-purple-600 dark:focus-within:text-purple-400 basis-4/5"
-                >
+                <span className="text-gray-700 dark:text-gray-400 pt-3 basis-1/6">
+                  Graduate
+                </span>
+                <div className="relative text-gray-500 focus-within:text-purple-600 dark:focus-within:text-purple-400 basis-4/5">
                   <input
-                    className="block w-full mt-1 text-sm text-black dark:text-gray-300 dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:focus:shadow-outline-gray form-input"
+                    className="block rounded-lg w-full mt-1 text-sm text-black dark:text-gray-300 dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:focus:shadow-outline-gray form-input"
                     placeholder="Graduate"
                   />
-                
                 </div>
               </label>
-
-              
-
-             
-            </div>
+              <div className="justify-end pt-8">
+                <button
+                  type="button"
+                  className=" block px-4 py-2 text-sm font-medium leading-5 text-white transition-colors duration-150 bg-purple-600 border border-none rounded-md active:bg-purple-600 hover:bg-purple-700 focus:outline-none focus:shadow-outline-purple"
+                >
+                  Save
+                </button>
+              </div>
             </div>
           </div>
-          </div>
-          </>
-  )
-}
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default AddMentee
+export default AddMentee;
